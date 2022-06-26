fcfdc03db873d1dc6bf687ef0cbcdd19b4976bb9
ASESIBO <cozzanibautista@gmail.comgit config --global user.email cozzanibautista@gmail.com>
2022-06-26 17:02:37 -0300

    creacion del home con HTML

---
+++ proyectointegrador-main/app.js
@@ -0,0 +1,19 @@
+const express=require('express');
+const app=express();
+const port=3100;
+const { dirname } = require('path');
+const path=require('path');
+const rutaAbsoluta='./views/';
+app.use(express.static('public'));
+
+
+app.get('/home', (req, res)=>{
+    const htmlPath=path.resolve(__dirname,rutaAbsoluta+'home.html');
+    res.sendFile(htmlPath)
+});
+
+app.get('/registro', (req, res)=>{
+    const htmlPath=path.resolve(__dirname,rutaAbsoluta+'registro.html');
+    res.sendFile(htmlPath)
+});
+app.listen(port, ()=>console.log("funcionando en localhost:"+port));
